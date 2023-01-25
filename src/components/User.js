43f18910beacd12d6c6b731ea6d17f963f6cda85
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-01-25 01:23:14 +0100

    user js files

---
+++ src/components/User.js
@@ -0,0 +1,16 @@
+
+import styles from "./User.module.css"
+
+const User = () => {
+    return (
+        <div className={styles.boxes}>
+            <div className={styles.containter}>
+                <div className={styles.inside}>max (31 years old)</div>
+                <div className={styles.inside}>max (31 years old)</div>
+                <div className={styles.inside}>max (31 years old)</div>
+            </div>
+        </div>
+    )
+}
+
+export default User
