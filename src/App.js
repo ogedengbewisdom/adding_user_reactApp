6e9dc7b5030fa9a1701d3d2c9e5fe71875e54d60
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-01-26 22:16:31 +0100

    add array of dummy data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import User from './components/User';
-
+import UserBackground from './components/UserBackground';
 
 function App() {
+
+  const users = [
+    {name: "Max", age: 31},
+    {name: "Wisdom", age: 28},
+    {name: "Mercy", age: 13},
+    {name: "Grace", age: 4}
+  ]
   return (
     <div>
-      <User />
+      <UserBackground
+       item={users}
+      />
     </div>
   );
 }
